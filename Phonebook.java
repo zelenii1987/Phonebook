abc292155a4815d4298788127162120d6a4d614b
zelenii1987 <90542767+zelenii1987@users.noreply.github.com>
2024-05-20 21:25:26 +0300

    Create Phonebook.java

---
+++ Phonebook.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Phonebook extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Phonebook.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Phonebook");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+}
